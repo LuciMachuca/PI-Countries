789358530267690605978d0bdceabf157f1faf57
Luci3329 <lmachuca3329>
2022-01-30 11:13:23 -0300

    ultima modificacion en el index

--- api/index.js
+++ api/index.js
@@ -17,37 +17,39 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const axios = require('axios');
 const { Country } = require('./src/db')
 
-let dataCountries = async () => {
+let dataCountries = async () => {  
   try {
-    const countries = await axios.get(`https://restcountries.com/v3.1/all`);
-    countries.data.map(async c => {
-      try {
-        var countryCreated = await Country.findOrCreate({
-          where: {
-            id: c.cca3,
-            name: c.name.common,
+    const countries = await axios.get("https://restcountries.com/v3.1/all");
+    const { data } = countries;
+    
+    data.map(async (c) => {
+      await Country.findOrCreate({
+    
+        where: { name: c.name.common },
+        defaults: {
+            id: c.cca3,  
+            name: c.name.common,  
             imgbandera: c.flags.png,
             continents: c.region,
             capital: c.capital ? c.capital[0] : 'no hay capital',
-            subregion: c.subregion ? c.subregion : 'no hay subregion',
-            area: parseInt(c.area),
-            population: parseInt(c.population)
+          subregion: c.subregion ? c.subregion : 'no hay subregion',
+            area: c.area,
+            population: c.population
           }
         })
-        return countryCreated;
-      } catch (err){
+        return data;
+      })
+    } catch (err){ 
         console.log(err);
       }
-    });
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+    }
+  
 
 dataCountries();
 
